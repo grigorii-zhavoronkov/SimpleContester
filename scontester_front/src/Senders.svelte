c3332e3ddb20198ae448e423234c62af5852de90
grigoriy <grisha.56@icloud.com>
2021-02-04 22:25:57 +0300

    Start migration from react to svelte

---
+++ scontester_front/src/Senders.svelte
@@ -0,0 +1,25 @@
+<script>
+    let senders = [
+        {UID: 'ABC11', name: 'Syka'},
+        {UID: 'AAA11', name: 'Blyat'}
+    ]
+</script>
+
+<main>
+    <table>
+        <thead>
+        <tr>
+            <td>UID</td>
+            <td>Name</td>
+        </tr>
+        </thead>
+        <tbody>
+        {#each senders as sender}
+            <tr>
+                <td>{sender.UID}</td>
+                <td>{sender.name}</td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
+</main>
---
+++ scontester_front/src/TaskTable.svelte
@@ -0,0 +1,23 @@
+<script>
+    let tasks = [
+        {id: 1, title: 'a+b'},
+        {id: 2, title: 'a+b v2'}
+    ]
+</script>
+
+<main>
+    <table>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+        </tr>
+        </thead>
+        {#each tasks as task (task.id)}
+            <tr>
+                <td><a>{task.id}</a></td>
+                <td>{task.title}</td>
+            </tr>
+        {/each}
+    </table>
+</main>
