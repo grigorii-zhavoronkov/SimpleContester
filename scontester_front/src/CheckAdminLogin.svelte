812d7b27381e9d178ff4cc29776e2afb5c6a1f5f
grigoriy <grisha.56@icloud.com>
2021-02-22 15:21:57 +0300

    Svelte migration

---
+++ scontester_front/src/CheckAdminLogin.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {onMount} from "svelte";
+    import {push} from "svelte-spa-router";
+    import axios from "axios";
+
+    onMount(() => {
+        const password = localStorage.getItem("password");
+        const UID = localStorage.getItem("UID");
+        if (password) {
+            axios.get("admin/api/login", {
+                headers: {
+                    "Authorization": password
+                }
+            })
+            .then(r => {
+                if (r.status !== 200) {
+                    push("/")
+                }
+            })
+            .catch(e => {
+                push("/")
+            })
+        } else if (UID) {
+
+        } else {
+            push("/");
+        }
+    })
+</script>
